6d9e5bbff0bf477ff0bd2b9cc3e242394ac8bb86
DevExpressExampleBot <fakeEmail>
2022-12-18 15:01:39 +0400

    Source auto update [skip ci]

---
+++ VB/DXMap_UseTitleOptions/App.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+
+Namespace DXMap_UseTitleOptions
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
